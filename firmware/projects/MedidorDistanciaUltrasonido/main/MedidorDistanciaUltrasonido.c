f0254c33951a5ea24ba6792b4d6e8753c4a53b63
Ailushh <ailuwendler@gmail.com>
2024-04-04 11:04:19 -0300

    Se crea proyecto MedidorDistanciaUltrasonido perteneciente al Proyecto 2

---
+++ firmware/projects/MedidorDistanciaUltrasonido/main/MedidorDistanciaUltrasonido.c
@@ -0,0 +1,39 @@
+/*! @mainpage Medidor de Distancia por Ultrasonido
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * 
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 04/04/2024 | Creación del Documento		                 |
+ *
+ * @author Tatiana Ailen Wendler (ailuwendler@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
